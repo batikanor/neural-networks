e5ea2e0109b1f98b8124782809fc1bdae1f2ee59
Batikan <batikanor@gmail.com>
2021-11-03 21:40:54 +0300

    minor explanation

--- p3.py
+++ p3.py
@@ -20,6 +20,7 @@ print(np.shape(np.dot(weights, inputs)))
 # and we want them to be multiplied by 1 (becuase the first weight for every neuron corresponds to the weight for the first input.) 
 # simple matrix mult.
 # and so on
-# see nnfs_trial_1\attachments\photo_2021-11-03_21-31-21.jpg for info
+# see nnfs_trial_1\attachments\photo_2021-11-03_21-31-21.jpg for math demonstration
+# but easier way to read dot products (matr x vector) in code is to just iterate through code lines of (or arrays within) matrix and dot product them withh elements of vector (so first iteration would be 0.2 * 1 + 0.8 * 2 + . + .  and so on)
 output = np.dot(weights, inputs) + biases
 print(output)
