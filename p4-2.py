86cf7d08a8fe7fb6e921b80f9ab594b66780a109
Batikan <batikanor@gmail.com>
2021-11-04 16:12:29 +0300

    multilayer neural networks, incomplete

---
+++ p4-2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# X is for inputs (input feature sets are conventionally denoted with X)
+X = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+]
+
+
+class Layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
+
+# when you save a model you're saving the weights and biases
+# when you load, you simply load them
+# but for initialization, you initialize them with rnadom values but from a certrain range, mostly small values etc
